6fd10741106d0636adb9e4ab0d994498b6eeffc7
Mark Todisco <marktodisco@gmail.com>
2020-10-14 18:42:11 -0400

    Update all

--- merge_pdfs.py
+++ merge_pdfs.py
@@ -4,13 +4,19 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
 
 
-# Get file names from 'pdfs' dir
-Tk().withdraw()
-files = askopenfilenames(initialdir='./', title='Chose files to merge', filetypes=(('PDF', '*.pdf'), ('PDF files', '*.pdf')))
+def run():
+    # Get file names from 'pdfs' dir
+    Tk().withdraw()
+    files = askopenfilenames(initialdir='./', title='Chose files to merge', filetypes=(('PDF', '*.pdf'), ('PDF files', '*.pdf')))
 
-# Create merger object
-if files == '':
-    print('Program stopped by user.')
-else:
-    output = os.path.join(os.getcwd(), 'merged.pdf')
-    merger = PDFMerger(files, output)
+    # Create merger object
+    if files == '':
+        print('Program stopped by user.')
+    else:
+        save_path = os.path.join(r'C:\\Users\\markt\\OneDrive\\Desktop', 'merged.pdf')
+        # print(save_path)
+        merger = PDFMerger(files, save_path)
+
+
+if __name__ == '__main__':
+    run()

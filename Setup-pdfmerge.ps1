309f2ddbaf68d974426f55f3ba95a197c0bfa903
Mark Todisco <marktodisco@gmail.com>
2021-09-12 16:14:31 -0400

    Delete setup powershell script

--- Setup-pdfmerge.ps1
+++
@@ -1,9 +0,0 @@
-param ($envName, $filePath)
-
-if ((-not [string]::IsNullOrEmpty("$envName")) -and (-not [string]::IsNullOrEmpty("$filePath"))) {
-    conda env create -n "$envName" -f "$filePath"
-    conda activate "$envName"
-    pip install -e .
-} else {
-    Write-Error 'Both parameters "envName" and "filePath" must be specified.'
-}

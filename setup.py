3d9ff733da407e80522a1ddaa6558e040a9f3ec4
Mark Todisco <marktodisco@gmail.com>
2021-09-12 16:16:56 -0400

    Updated package setup files and metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from setuptools import setup, find_packages
+import setuptools
 
-
-setup(
-    name='pdfmerge',
-    packages=find_packages(),
-    entry_points=dict(
-        console_scripts=['pdfmerge=pdfmerge.pdfmerge:main']
-    )
+setuptools.setup(
+    name="pdfmerge",
+    version="0.0.1",
+    author="Mark Todisco",
+    description="CLI tool for merging PDF documents",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    pakage_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    entry_points={
+        "console_scripts": [
+            "pdfmerge=pdfmerge.pdfmerge:main",
+        ],
+    },
 )
